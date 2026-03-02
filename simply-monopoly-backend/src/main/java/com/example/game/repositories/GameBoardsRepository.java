d055e5185f543e7b5b408f0e2d9cab75c5a155cb
CodyRR <codyrobinson28@gmail.com>
2026-03-01 19:11:27 -0600

    Creating controller for GameBoards.  I'm not sure if they all will be used.

--- simply-monopoly-backend/src/main/java/com/example/game/repositories/GameBoardsRepository.java
+++ simply-monopoly-backend/src/main/java/com/example/game/repositories/GameBoardsRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameBoardRepository extends JpaRepository<GameBoards, Integer> {
+public interface GameBoardsRepository extends JpaRepository<GameBoards, Integer> {
 }
